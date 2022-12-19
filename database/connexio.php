828fd83c79d951a8341fe36fd7853e402ec0e0f0
MarcGarcia03 <100956036+MarcGarcia03@users.noreply.github.com>
2022-12-19 15:41:20 +0100

    prova

--- database/connexio.php
+++ database/connexio.php
@@ -2,14 +2,13 @@
 #FUNCIÓ PER REALITZAR LA CONNEXIÓ A LA BD
 function connectarBD($db_host, $db_usuari, $db_password, $db_nom)
 {
-    try {
-        $DB = new PDO("mysql:host=$db_host; dbname=$db_nom;charset=utf8", $db_usuari, $db_password);
-        return $DB;
+   try {
+      $DB = new PDO("mysql:host=$db_host; dbname=$db_nom;charset=utf8", $db_usuari, $db_password);
+      return $DB;
       } catch (PDOException $e) {
-        echo "Error al conectarse a la base de dades.";
-        die();
+      echo "Error al conectarse a la base de dades.";
+      die();
       }
-    
 }
 #VARIABLES PER LA CONNEXIÓ A LA BASE DE DADES
 $db_host = "localhost";
